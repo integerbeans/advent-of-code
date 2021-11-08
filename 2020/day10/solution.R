e112eb076db1075d2d06cedee070e42d7b2a6004
integerbeans <w-gh@wmeyer.eu>
2021-11-08 21:12:59 +0100

    fixed formatting: 2020 - day 10, added: solution1 for 2020 - day 11

--- 2020/day10/solution.R
+++ 2020/day10/solution.R
@@ -6,3 +6,4 @@ rudolph <- sort(c(0, rudolph, max(rudolph) + 3))
 paste(tree,format(prod(rle(sort(diff(rudolph)))$lengths), scientific = FALSE))
 reindeer <- rle(diff(rudolph))
 paste(tree,format(prod(c(1,2,4,7)[reindeer$lengths[reindeer$values == 1]]),scientific = FALSE))
+
